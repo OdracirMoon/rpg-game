13c85fbaf44e53a12b15f17c3734a18c371cf878
OdracirMoon <124225207+OdracirMoon@users.noreply.github.com>
2026-03-06 10:36:52 -0600

    Add new player attributes for RPG mechanics

--- game/js/state.js
+++ game/js/state.js
@@ -11,15 +11,52 @@ export const gameState = {
     player: { 
         x: 16, y: 25,
         hp: 0, mp: 0, ep: 0, xp: 0, gold: 0, potions: 3, manaPotions: 1, energyPotions: 1, level: 1,
-        baseMaxHp: 0, baseMaxMp: 0, baseMaxEp: 0, baseAtk: 0, baseDef: 0, baseMag: 0, weapon: null, armor: null, playerClass: '',
-        mapImg: 'img/player/heroe.png', combatImg: 'img/player/heroe.png',
+        
+        // IDENTIDAD (Nuevo sistema Rol -> Personaje)
+        role: '', 
+        characterId: '', 
+        characterName: '',
+        mapImg: 'img/player/heroe.png', 
+        combatImg: 'img/player/heroe.png',
+
+        // --- ESTADÍSTICAS BASE (Estilo MOBA) ---
+        // Supervivencia
+        baseMaxHp: 0, baseHpRegen: 0,
+        baseMaxMp: 0, baseMpRegen: 0,
+        baseMaxEp: 0, baseEpRegen: 0,
+        baseArmor: 0, baseMagicResist: 0,
+        baseTenacity: 0, // Porcentaje de reducción de CC (0 a 100)
+
+        // Daño
+        baseAd: 0, // Attack Damage (Físico)
+        baseAp: 0, // Ability Power (Mágico)
+        baseCritChance: 0, // 0.0 a 1.0
+        baseCritDamage: 1.75, // Multiplicador base (175%)
+
+        // Velocidad
+        baseAttackSpeed: 1.0, 
+        baseAbilityHaste: 0, 
+        baseMoveSpeed: 300,
+
+        // Sustento
+        baseLifeSteal: 0, // Porcentaje de curación en ataques básicos
+        baseOmnivamp: 0,  // Porcentaje de curación en todo el daño
+
+        // Penetración
+        baseArmorPen: 0, // Porcentaje (ej. 0.30 ignora 30% armadura)
+        baseLethality: 0, // Plana (ej. 10 ignora 10 puntos de armadura directos)
+        baseMagicPen: 0, // Porcentaje o plana para daño mágico
+
+        baseRange: 1, // 1 es cuerpo a cuerpo, 3+ es a distancia
+
+        // Inventario y Progresión
+        weapon: null, armor: null,
         inventory: { weapons: [], armors: [] },
         zoneQuestProgress: [0, 0, 0, 0, 0, 0],
         hasKey: { 0: false, 1: false, 2: false, 3: false, 4: false, 5: false },
         
         statPoints: 0,
-        skillPoints: 0, // NUEVA VARIABLE: SP para el Árbol
-        
+        skillPoints: 0, 
         knownSkills: [], 
         equippedSkills: { special: null, defensive: null }
     },
